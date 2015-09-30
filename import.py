8027b1be36d01181f8623d90cd66dd2f1f12f85b
tkyanko <tkyanko@gmail.com>
2015-09-29 20:41:56 -0400

    Cleaned up the XML file slightly.  Added some documentation and cleaned print functions from parse().

--- import.py
+++ import.py
@@ -5,7 +5,12 @@ INPUT_FILE = 'jans.xml'
 relations = dict()
 jans = dict()
 
+
 def parse():
+    """
+    Reads the XML file and fills the dictionaries with the JANS and their
+    relations.
+    """
     tree = ElementTree.parse(INPUT_FILE)
     root = tree.getroot()
 
@@ -13,9 +18,6 @@ def parse():
         jans[child.attrib['name']] = child
         relations[child.attrib['name']] = \
           [name.attrib['name'] for name in child.findall("related")]
-
-    print(jans)
-    print(relations)
 
 
 if __name__ == '__main__':
