3d73082ad5a68ced99991a6d7a1f7074802b3fc2
tkyanko <tkyanko@gmail.com>
2015-09-29 20:19:15 -0400

    Able to add the jans to a dictionary.

--- import.py
+++ import.py
@@ -2,8 +2,17 @@ import xml.etree.ElementTree as ElementTree
 
 INPUT_FILE = 'jans.xml'
 
+relations = dict()
+jans = dict()
+
 def parse():
     tree = ElementTree.parse(INPUT_FILE)
+    root = tree.getroot()
+
+    for child in root:
+        jans[child.attrib['name']] = child
+
+    print(jans)
 
 
 if __name__ == '__main__':
